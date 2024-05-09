f7a6e42a375c3fcb69d53532a4820b07a5dab240
Schumannyeah <schumannyeah@hotmail.com>
2024-05-09 16:59:32 +0800

    OpEx_for_main_2024050903_commit

--- C4_Intermediate_ML/kaggle_c4.py
+++ C4_Intermediate_ML/kaggle_c4.py
@@ -3,12 +3,14 @@
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 
 # Read the data
 X_full = pd.read_csv('../dataset/train.csv', index_col='Id')
 X_test_full = pd.read_csv('../dataset/test.csv', index_col='Id')
 
-# print(X_full.head())
+print(X_test_full.head())
 
 # Obtain target and predictors
 y = X_full.SalePrice
@@ -17,5 +19,33 @@ X = X_full[features].copy()
 X_test = X_test_full[features].copy()
 
 # Break off validation set from training data
+# Training set (X_train, y_train): This will be used to train your machine learning model.
+# It represents 80% of the original data (specified by train_size=0.8).
+# Validation set (X_valid, y_valid): This will be used to evaluate the performance of your model on unseen data.
+# It represents the remaining 20% (specified by test_size=0.2).
+# random_state=0: This argument sets a seed for the random number generator used to split the data.
+# Using a seed ensures reproducibility, meaning you'll get the same split each time you run the code.
+# This is helpful for debugging and comparing different models.
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
                                                       random_state=0)
+
+# print(X_train.head())
+
+# Define the models
+model_1 = RandomForestRegressor(n_estimators=50, random_state=0)
+model_2 = RandomForestRegressor(n_estimators=100, random_state=0)
+model_3 = RandomForestRegressor(n_estimators=100, criterion='absolute_error', random_state=0)
+model_4 = RandomForestRegressor(n_estimators=200, min_samples_split=20, random_state=0)
+model_5 = RandomForestRegressor(n_estimators=100, max_depth=7, random_state=0)
+
+models = [model_1, model_2, model_3, model_4, model_5]
+
+# Function for comparing different models
+def score_model(model, X_t=X_train, X_v=X_valid, y_t=y_train, y_v=y_valid):
+    model.fit(X_t, y_t)
+    preds = model.predict(X_v)
+    return mean_absolute_error(y_v, preds)
+
+for i in range(0, len(models)):
+    mae = score_model(models[i])
+    print("Model %d MAE: %d" % (i+1, mae))
